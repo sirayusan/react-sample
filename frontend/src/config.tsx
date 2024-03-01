d5c28e88e4a51cfa7125076f95c7ed05ffeeed15
endo\endo2 <sirayusan3636@gmail.com>
2024-03-02 03:17:19 +0900

    #7 チャット画面作成

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,16 +1,21 @@
 // 開発環境用の設定
+const devApiUrl = process.env.NODE_ENV === 'SSL_DEV' ? "https://localhost:443" : "https://localhost:8080";
+const devWsUrl = process.env.NODE_ENV === 'SSL_DEV' ? "wss://localhost:443" : "ws://localhost:8080";
 const devConfig = {
-  API_URL: "http://localhost:8080",
-  WEBSOCKET_URL: "ws://localhost:8080",
+  API_URL: devApiUrl,
+  WEBSOCKET_URL: devWsUrl,
 };
-  
+
 // 本番環境用の設定
+const proApiUrl = process.env.NODE_ENV === 'SSL_DEV' ? "https://localhost:443" : "https://localhost:8080";
+const proWsUrl = process.env.NODE_ENV === 'SSL_DEV' ? "wss://localhost:443" : "ws://localhost:8080";
+
 const prodConfig = {
-  API_URL: "https://api.example.com",
-  WEBSOCKET_URL: "wss://api.example.com",
+  API_URL: proApiUrl,
+  WEBSOCKET_URL: proWsUrl,
 };
-  
+
 // 環境に応じた設定の選択
-const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+const config = (process.env.NODE_ENV === "SSL_PRO" || process.env.NODE_ENV === "PRO") ? prodConfig : devConfig;
 
 export default config;
