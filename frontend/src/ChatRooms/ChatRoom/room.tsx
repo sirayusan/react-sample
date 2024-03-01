d7dffc2d499171b5875adab8b01071ac158aa32f
endo\endo2 <sirayusan3636@gmail.com>
2024-03-01 19:05:16 +0900

    #7 チャット画面作成

--- frontend/src/ChatRooms/ChatRoom/room.tsx
+++ frontend/src/ChatRooms/ChatRoom/room.tsx
@@ -1,19 +1,22 @@
 import  { useState, useEffect } from 'react';
 import { useParams, Link } from "react-router-dom";
+import Config from '../../config';
+import Cookies from 'js-cookie';
 
 // メッセージの型を定義
 interface Message {
+    sender_user_id: number;
     user_name: string;
     message: string;
     created_at: string;
 }
 
-function Chat() {
+export default function Chat() {
     const [messages, setMessages] = useState<Message[]>([]); // Message型の配列に変更
     const [messageToSend, setMessageToSend] = useState('');
     const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+    const [isError, setIsError] = useState(true);
     const { id } = useParams();
-
     const formatDate = (dateString: string) => {
         const options: Intl.DateTimeFormatOptions = {
             year: 'numeric', month: '2-digit', day: '2-digit',
@@ -23,9 +26,13 @@ function Chat() {
     };
 
     useEffect(() => {
-        const ws = new WebSocket(`ws://localhost:8080/chats/${id}`);
+        const jwt = Cookies.get('jwt');
+        const ws = new WebSocket(`${Config.WEBSOCKET_URL}/chats/${id}?jwt=${jwt}`);
 
-        ws.onopen = () => console.log('WebSocket接続が確立されました。');
+        ws.onopen = () => {
+            setIsError(false)
+            console.log('WebSocket接続が確立されました。');
+        }
         ws.onmessage = (event) => {
             const message = JSON.parse(event.data); // JSON形式のメッセージをパース
             console.log(message); // 受信したメッセージオブジェクトをコンソールに表示
@@ -37,6 +44,10 @@ function Chat() {
 
         return () => ws.close();
     }, [id]);
+
+    if (isError) {
+        return <div>データの読み込み中。</div>;
+    }
 
     const sendMessage = () => {
         if (webSocket && messageToSend) {
@@ -50,11 +61,11 @@ function Chat() {
             <Link to="/chat-rooms">戻る</Link>
             <h2>チャット</h2>
             <div>
-                {messages.map((message) => (
-                    <>
-                        <p>送信者:{message.user_name} 送信時間: {formatDate(message.created_at)}</p>
+                {messages.map((message,index) => (
+                    <div key={index}>
+                        <p>送信者:{message.user_name} ID:{message.sender_user_id} 送信時間: {formatDate(message.created_at)}</p>
                         <p>{message.message}</p>
-                    </>
+                    </div>
                 ))}
             </div>
             <input
@@ -67,5 +78,3 @@ function Chat() {
         </div>
     );
 }
-
-export default Chat;
--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,14 +1,16 @@
 // 開発環境用の設定
 const devConfig = {
-    API_URL: "http://localhost:8080",
-  };
+  API_URL: "http://localhost:8080",
+  WEBSOCKET_URL: "ws://localhost:8080",
+};
   
-  // 本番環境用の設定
-  const prodConfig = {
-    API_URL: "https://api.example.com",
-  };
+// 本番環境用の設定
+const prodConfig = {
+  API_URL: "https://api.example.com",
+  WEBSOCKET_URL: "wss://api.example.com",
+};
   
-  // 環境に応じた設定の選択
-  const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
-  
-  export default config;
+// 環境に応じた設定の選択
+const config = process.env.NODE_ENV === "production" ? prodConfig : devConfig;
+
+export default config;
